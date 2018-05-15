54358532fcc88f91f053d5572781fc7acb20c975
James D. Bartlett III <jimmyb@gmail.com>
2018-05-14 22:44:44 -0500

    Gulp Setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp');
+var exec = require('gulp-exec');
 
 gulp.task('default', function() {
-  // place code for your default task here
+  console.log('Running default gulp function...')
+});
+
+gulp.task('deploy', function() {
+	console.log('Copying files from dev to dist...');
+	exec('cp dev/index.html dist/index.html');
+	exec('cp dev/css/_styles.css dist/css/styles.css');
+	exec('cp dev/js/_app.js dist/js/app.js');
+	exec('cp -R dev/img dist/img');
+  console.log('Files copied! Dist is now live!');
 });
