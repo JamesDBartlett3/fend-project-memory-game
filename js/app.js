cc9400bb788be6067d0179fcbf2aab6b81fe6a7c
James D. Bartlett III <jimmyb@gmail.com>
2018-05-28 05:50:25 -0500

    deck building from array

--- js/app.js
+++ js/app.js
@@ -8,61 +8,61 @@ let moves = 0;
 let matches = 0;
 let timer = 0;
 let rating = 3;
-let oneStar = "🌟";
-let twoStar = "🌟🌟";
-let threeStar = "🌟🌟🌟";
 
-// HTML Content Vars
+// Glyphs & Icons
+let glyphs = [];
+let icons = [];
+
+// HTML Content
 const dc = '<div class="deck-container"></div>';
 const d = '<div class="deck"></div>';
-const r1 = '<div id="r1" class="row"></div>';
-const r2 = '<div id="r2" class="row"></div>';
-const r3 = '<div id="r3" class="row"></div>';
-const r4 = '<div id="r4" class="row"></div>';
-const c1 = '<div id="c1" class="card"></div>';
-const c2 = '<div id="c2" class="card"></div>';
-const c3 = '<div id="c3" class="card"></div>';
-const c4 = '<div id="c4" class="card"></div>';
+const star = '<li><i class="fa fa-star"></i></li>'
 
-// jQuery Vars
-const gameSurface = $('.game-surface');
 
 /*
-Function instantiation
+Functions
 */
 
-
-function loadGlyphs() {
-  let glyphs = []
+function buildContainer () {
+  const gameSurface = $('.game-surface');
+  gameSurface.append(dc);
+  $('.deck-container').append(d);
+  gameSurface.toggleClass('hidden');
 }
 
+function getGlyphs() {
+  $.get("./glyphs.txt", function(data) {
+    glyphs = data.split(',');
+  }, "text");
+}
 
+function pickIcons() {
+  icons = [
+    glyphs[0],glyphs[0],glyphs[1],glyphs[1],
+    glyphs[2],glyphs[2],glyphs[3],glyphs[3],
+    glyphs[4],glyphs[4],glyphs[5],glyphs[5],
+    glyphs[6],glyphs[6],glyphs[7],glyphs[7]
+  ]
+}
 
+function deployCards() {
+  pickIcons();
+  icons = shuffle(icons);
+  for (let r = 0; r < 4; r++) {
+    let row = $('<div class="row"></div>');
+    for (let c = 0; c < 4; c++) {
+      let card = $('<div class="card"></div>').attr('id', r + "_" + c);
+      let s = r + c;
+      card.append("<img src='" + icons[s] +"'>");
+      row.append(card);
+    }
+    $('.deck').append(row);
+  }
+}
 
-$(document).ready(function() {
-  /*
-  stuff to do when page loads
-  */
+function updateScore() {
 
-  gameSurface.append(dc);
-  const deckContainer = $('.deck-container');
-  deckContainer.append(d);
-  const deck = $('.deck');
-  deck.append(r1 + r2 + r3 + r4);
-  const row = $('.row');
-  row.append(c1 + c2 + c3 + c4);
-  gameSurface.toggleClass('hidden');
-
-
-});
-
-/*
-* Display the cards on the page
-*   - shuffle the list of cards using the provided "shuffle" method below
-*   - loop through each card and create its HTML
-*   - add each card's HTML to the page
-*/
-
+}
 
 function flip(event) {
 
@@ -72,22 +72,22 @@ function check(event) {
 
 }
 
-
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+function reload() {
+  location.reload();
 }
+
+
+
+/*
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method below
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*/
+
+
+
+// Event Listeners:
 
 
 
@@ -101,3 +101,28 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+ // Shuffle function from http://stackoverflow.com/a/2450976
+ function shuffle(array) {
+     var currentIndex = array.length, temporaryValue, randomIndex;
+
+     while (currentIndex !== 0) {
+         randomIndex = Math.floor(Math.random() * currentIndex);
+         currentIndex -= 1;
+         temporaryValue = array[currentIndex];
+         array[currentIndex] = array[randomIndex];
+         array[randomIndex] = temporaryValue;
+     }
+
+     return array;
+ }
+
+
+ /*
+ stuff to do when page loads
+ */
+ getGlyphs();
+ glyphs = shuffle(glyphs);
+ buildContainer();
+ deployCards();
